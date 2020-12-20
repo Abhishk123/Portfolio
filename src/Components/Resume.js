228f304871da07b0a72b00c76807ea37ef576a35
ABHISHEK URS C J <abhishekurst@gmail.com>
2020-12-20 23:53:57 +0530

    Router added

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,4 +1,65 @@
 import React from 'react';
+import react from '../assets/icons/react.svg'
+import python from '../assets/icons/python.svg'
+import Bar from '../Components/Bar'
+
+
+const languages=[
+    {
+        icon:react,
+        name:'C',
+        level: '75'
+    },
+    {
+        icon:react,
+        name:'Java',
+        level: '65'
+    },
+    {
+        icon:react,
+        name:'HTML',
+        level: '75'
+    },
+    {
+        icon:react,
+        name:'CSS',
+        level: '60'
+    },
+    {
+        icon:python,
+        name:'Python',
+        level: '60'
+    },
+    {
+        icon:react,
+        name:'Javascript',
+        level: '50'
+    }
+]
+
+
+const tools=[
+    {
+        icon:react,
+        name:'Android Studio',
+        level: '70'
+    },
+    {
+        icon:react,
+        name:'Eclipse',
+        level: '60'
+    },
+    {
+        icon:react,
+        name:'MS Word',
+        level: '75'
+    },
+    {
+        icon:python,
+        name:'Pycharm',
+        level: '40'
+    }
+]
 
 const Resume = () => {
     return (
@@ -15,22 +76,25 @@ const Resume = () => {
                         >Computer Science and Engineering
                         </h5>
                         <p className='resume-card_name'>
-                            BGS Institute of Technology
+                            BGS Institute of Technology(2017-2021)
                         </p>
-                        <p className="resume-card_details">Lorem ipsum, dolor sit amet consecteturibusdam et vitae natus dolores! Aliquid, a
+                        <p className="resume-card_details">I am currently persuing B.E in Computer Science and
+                        Engineering from BGS Institute of Technology.
                         </p>
                     </div>
                     </div>
 
                     <div className='col-lg-6 resume-card'>
                     <h4 className="resume-card_heading">
-                        Experience
+                        Career Objective
                     </h4>
 
                     <div className="resume-card_body">
-                        <h5 className='resume-card_title'
-                        >Fresher
-                        </h5>
+                        <p className='resume-card_title'
+                        >To gain good experience in the software field, to develop more and more societal
+                        useful applications. To solve the real world problems using existing technology and
+                        to work with the well experienced team in a good position.
+                        </p>
                        
                     </div>
                     </div>
@@ -42,10 +106,12 @@ const Resume = () => {
 
                         <div className="resume-language_heading">
                             <h5 className="resume-language_heading">
-                                Language and Framework
+                                Technical Skills
                             </h5>
-                            <div className="resume-language_body">
+                            <div className="resume-language_body mt-3">
                                 {
+                                    languages.map(language=>
+                                        <Bar value={language}/>)
 
                                 }
                             </div>
@@ -56,10 +122,13 @@ const Resume = () => {
 
                         <div className="resume-language_heading">
                           <h5 className="resume-language_heading">
-                               Language and Framework
+                               Tools and Softwares
                           </h5>
-                      <div className="resume-language_body">
+                      <div className="resume-language_body mt-3">
                          {
+                             tools.map(tool=>
+                               <Bar value={tool}/>
+                                )
 
                          }
                       </div>
